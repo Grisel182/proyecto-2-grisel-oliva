4e3335f3038159e4d41444c48f929477d06d4b21
Grisel182 <grisel.oliva182@gmail.com>
2022-07-19 00:02:42 -0300

    commit real

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const menuItems = document.querySelectorAll('.menu-items');
+const menuBtn = document.querySelector('#menu-button');
+
+menuBtn.addEventListener('click', function() {
+    document.body.classList.toggle('mobile-menu-active')
+});
+
+
+menuItems.forEach(function(item){
+
+    item.addEventListener('click', function() {
+        document.body.classList.remove ('mobile-menu-active');
+        const currentItem = document.querySelector('.active');
+        currentItem.classList.remove('active');
+        this.classList.add('active');
+    });
+});
